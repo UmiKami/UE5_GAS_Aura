ba02b7123253213a3135f2af6d78c6a0c27bb067
UmiKami <ggoku02@gmail.com>
2025-09-13 15:04:39 -0400

    added init setup for AuraCharacter

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -3,3 +3,27 @@
 
 #include "Character/AuraCharacter.h"
 
+#include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+AAuraCharacter::AAuraCharacter()
+{
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>("Spring Arm");
+
+	SpringArm->SetupAttachment(GetRootComponent());
+	
+	Camera = CreateDefaultSubobject<UCameraComponent>("Camera");
+
+	Camera->SetupAttachment(SpringArm);
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 400.f, 0.f);
+	GetCharacterMovement()->bConstrainToPlane = true;
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+}
+
--- Source/Aura/Public/Character/AuraCharacter.h
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -6,6 +6,8 @@
 #include "Character/AuraCharacterBase.h"
 #include "AuraCharacter.generated.h"
 
+class USpringArmComponent;
+class UCameraComponent;
 /**
  * 
  */
@@ -13,5 +15,13 @@ UCLASS()
 class AURA_API AAuraCharacter : public AAuraCharacterBase
 {
 	GENERATED_BODY()
+
+public:
+	AAuraCharacter();
+protected:
+	UPROPERTY(EditAnywhere, Category="Camera")
+	TObjectPtr<UCameraComponent> Camera;
 	
+	UPROPERTY(EditAnywhere, Category="Camera")
+	TObjectPtr<USpringArmComponent> SpringArm;
 };
