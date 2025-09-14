5060faf19cddda0dd1ff5e63cc0f2c69135f5807
UmiKami <ggoku02@gmail.com>
2025-09-14 10:39:45 -0400

    Added implementation of IEnemyInterface on AuraEnemy

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -3,3 +3,14 @@
 
 #include "Character/AuraEnemy.h"
 
+
+void AAuraEnemy::HighlightActor()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Highlight Actor"));
+	DrawDebugSphere(GetWorld(), GetActorLocation(), 20, 30, FColor::Purple, true);
+}
+
+void AAuraEnemy::UnHighlightActor()
+{
+	FlushPersistentDebugLines(GetWorld());
+}
