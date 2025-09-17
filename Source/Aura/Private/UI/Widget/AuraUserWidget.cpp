15a9bd52b180872903af59d2235d682b7f7282ac
UmiKami <ggoku02@gmail.com>
2025-09-17 18:39:17 -0400

    Base User widget that accepts any controller

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+﻿// Copyright By UmiKami
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
