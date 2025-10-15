7183e0de1327e7e0935124dfe2d17daf6fe6a439
UmiKami <ggoku02@gmail.com>
2025-10-14 20:37:44 -0400

    Max mana now uses a calculation script that takes into acccount player level AND intelligence points

---
+++ Source/Aura/Private/AbilitySystem/ModMagCalc/MMC_MaxMana.cpp
@@ -0,0 +1,38 @@
+﻿// Copyright By UmiKami
+
+
+#include "AbilitySystem/ModMagCalc/MMC_MaxMana.h"
+
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "Interaction/CombatInterface.h"
+
+UMMC_MaxMana::UMMC_MaxMana()
+{
+	IntelligenceDef.AttributeToCapture = UAuraAttributeSet::GetIntelligenceAttribute();
+	IntelligenceDef.AttributeSource = EGameplayEffectAttributeCaptureSource::Target;
+	IntelligenceDef.bSnapshot = false;
+
+	RelevantAttributesToCapture.Add(IntelligenceDef);
+}
+
+float UMMC_MaxMana::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const
+{
+	float Intelligence = 0.f;
+
+	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+
+	FAggregatorEvaluateParameters EvaluationParameters;
+	EvaluationParameters.SourceTags = SourceTags;
+	EvaluationParameters.TargetTags = TargetTags;
+
+	GetCapturedAttributeMagnitude(IntelligenceDef, Spec, EvaluationParameters, Intelligence);
+
+	Intelligence = FMath::Max(Intelligence, 0.f);
+
+	const TScriptInterface<ICombatInterface> CombatInterface = Spec.GetContext().GetSourceObject();
+
+	const int32 PlayerLevel = CombatInterface->GetPlayerLevel();
+
+	return 25.f + 2.f * Intelligence + 10.f * PlayerLevel;
+}
