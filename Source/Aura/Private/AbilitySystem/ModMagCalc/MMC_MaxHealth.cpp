2bbb4780dca128e6f39559f987b8905a29595bee
UmiKami <ggoku02@gmail.com>
2025-10-14 20:37:30 -0400

    Max health now uses a calculation script that takes into acccount player level AND vigor points

---
+++ Source/Aura/Private/AbilitySystem/ModMagCalc/MMC_MaxHealth.cpp
@@ -0,0 +1,40 @@
+﻿// Copyright By UmiKami
+
+
+#include "AbilitySystem/ModMagCalc/MMC_MaxHealth.h"
+
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "Interaction/CombatInterface.h"
+#include "Kismet/GameplayStatics.h"
+#include "Player/AuraPlayerState.h"
+
+UMMC_MaxHealth::UMMC_MaxHealth()
+{
+	// NOTE: GetVigorAttribute method is provided by the ATTRIBUTE_ACCESSORS macro on UAuraAttributeSet, it is not literally defined on UAuraAttributeSet
+	VigorDef.AttributeToCapture = UAuraAttributeSet::GetVigorAttribute();
+	VigorDef.AttributeSource = EGameplayEffectAttributeCaptureSource::Target;
+	VigorDef.bSnapshot = false;
+
+	RelevantAttributesToCapture.Add(VigorDef);
+}
+
+float UMMC_MaxHealth::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const
+{
+	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+
+	FAggregatorEvaluateParameters EvaluationParameters;
+	EvaluationParameters.SourceTags = SourceTags;
+	EvaluationParameters.TargetTags = TargetTags;
+
+	float Vigor = 0.f;
+	GetCapturedAttributeMagnitude(VigorDef, Spec, EvaluationParameters, Vigor);
+
+	Vigor = FMath::Max<float>(Vigor, 0.f);
+
+	TScriptInterface<ICombatInterface> CombatInterface = Spec.GetContext().GetSourceObject();
+
+	const int32 PlayerLevel = CombatInterface->GetPlayerLevel();
+
+	return 80.f + 2.5f * Vigor + 10.f * PlayerLevel;
+}
