de3bd61a8645f414742a4d31581593863cc5f7bf
UmiKami <ggoku02@gmail.com>
2025-10-24 10:08:17 -0400

    Implemented native tags for Secondary and Primary attributes

---
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -0,0 +1,86 @@
+﻿// Copyright By UmiKami
+
+
+#include "AuraGameplayTags.h"
+
+#include "GameplayTagsManager.h"
+
+namespace AuraGameplayTags
+{
+	// region Primary Attributes
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(
+		Attributes_Primary_Strength,
+		"Attributes.Primary.Strength",
+		"Increases physical damage."
+	);
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(
+		Attributes_Primary_Intelligence,
+		"Attributes.Primary.Intelligence",
+		"Increases magical damage."
+	);
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(
+		Attributes_Primary_Resilience,
+		"Attributes.Primary.Resilience",
+		"Increases Armor and Armor Penetration."
+	);
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(
+		Attributes_Primary_Vigor,
+		"Attributes.Primary.Vigor",
+		"Increases Max Health"
+	);
+	// endregion 
+
+	// region Secondary Attributes
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(
+		Attributes_Secondary_Armor,
+		"Attributes.Secondary.Armor",
+		"Decreases damage taken and increases chance of hit being blocked."
+	);
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(
+		Attributes_Secondary_ArmorPenetration,
+		"Attributes.Secondary.ArmorPenetration",
+		"Ignores percentage of enemy Armor, increases Crit Hit Chance."
+	);
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(
+		Attributes_Secondary_BlockChance,
+		"Attributes.Secondary.BlockChance",
+		"Chance to cut incoming damage in half."
+	);
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(
+		Attributes_Secondary_CriticalHitChance,
+		"Attributes.Secondary.CriticalHitChance",
+		"Chance to double damage plus critical hit bonus."
+	);
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(
+		Attributes_Secondary_CriticalHitDamage,
+		"Attributes.Secondary.CriticalHitDamage",
+		"Bonus damage added when a critical hit is scored."
+	);
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(
+		Attributes_Secondary_CriticalHitResistance,
+		"Attributes.Secondary.CriticalHitResistance",
+		"Reduces critical hit chance of attacking enemies."
+	);
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(
+		Attributes_Secondary_HealthRegeneration,
+		"Attributes.Secondary.HealthRegeneration",
+		"Amount of Health regenerated every 1 second."
+	);
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(
+		Attributes_Secondary_ManaRegeneration,
+		"Attributes.Secondary.ManaRegeneration",
+		"Amount of Mana regenerated every 1 second."
+	);
+
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(
+		Attributes_Secondary_MaxHealth,
+		"Attributes.Secondary.MaxHealth",
+		"Maximum amount of Health obtainable."
+	);
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(
+		Attributes_Secondary_MaxMana,
+		"Attributes.Secondary.MaxMana",
+		"Maximum amount of Health obtainable."
+	);
+	// endregion 
+}
---
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -0,0 +1,32 @@
+﻿// Copyright By UmiKami
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NativeGameplayTags.h"
+
+/**
+ * AureGameplayTags
+ *
+ * Singleton Containing Native Gameplay Tags
+ */
+
+namespace AuraGameplayTags
+{
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Attributes_Primary_Strength);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Attributes_Primary_Intelligence);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Attributes_Primary_Resilience);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Attributes_Primary_Vigor);
+
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Attributes_Secondary_Armor);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Attributes_Secondary_ArmorPenetration);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Attributes_Secondary_BlockChance);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Attributes_Secondary_CriticalHitChance);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Attributes_Secondary_CriticalHitDamage);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Attributes_Secondary_CriticalHitResistance);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Attributes_Secondary_HealthRegeneration);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Attributes_Secondary_ManaRegeneration);
+
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Attributes_Secondary_MaxHealth);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Attributes_Secondary_MaxMana);
+}
