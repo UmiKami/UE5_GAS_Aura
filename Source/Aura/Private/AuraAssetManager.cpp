db23dc4f1d78d49ef038255acb8348dd72a08e77
UmiKami <ggoku02@gmail.com>
2025-10-24 10:08:49 -0400

    Created Custom Asset Manager that allows us to implenment custom logic for assets management

---
+++ Source/Aura/Private/AuraAssetManager.cpp
@@ -0,0 +1,18 @@
+﻿// Copyright By UmiKami
+
+
+#include "AuraAssetManager.h"
+
+UAuraAssetManager& UAuraAssetManager::Get()
+{
+	checkf(GEngine, TEXT("GEngine not valid."));
+
+	UAuraAssetManager* AuraAssetManager = Cast<UAuraAssetManager>(GEngine->AssetManager);
+
+	return *AuraAssetManager;
+}
+
+void UAuraAssetManager::StartInitialLoading()
+{
+	Super::StartInitialLoading();
+}
