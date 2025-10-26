31c54fc6ee195cf93a4f4ce59e267d3ed1bdf119
UmiKami <ggoku02@gmail.com>
2025-10-26 17:12:02 -0400

    Create Ability System static library to get widget controllers anywhere from blueprints

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemFunctionLibrary.cpp
@@ -0,0 +1,48 @@
+﻿// Copyright By UmiKami
+
+
+#include "AbilitySystem/AuraAbilitySystemFunctionLibrary.h"
+
+#include "Player/AuraPlayerState.h"
+#include "UI/HUD/AuraHUD.h"
+#include "UI/Widget/AuraWidgetController.h"
+
+UOverlayWidgetController* UAuraAbilitySystemFunctionLibrary::GetOverlayWidgetController(
+	const UObject* WorldContextObject)
+{
+	if(APlayerController* PlayerController = WorldContextObject->GetWorld()->GetFirstPlayerController())
+	{
+		if (AAuraHUD* AuraHUD = Cast<AAuraHUD>(PlayerController->GetHUD()))
+		{
+			AAuraPlayerState* PS = PlayerController->GetPlayerState<AAuraPlayerState>();
+			UAbilitySystemComponent* ASC = PS->GetAbilitySystemComponent();
+			UAttributeSet* AS = PS->GetAttributeSet();
+
+			const FWidgetControllerParams WidgetControllerParams(PlayerController, PS, ASC, AS);
+
+			return AuraHUD->GetOverlayWidgetController(WidgetControllerParams);
+		}
+	}
+
+	return nullptr;
+}
+
+UAttributeMenuWidgetController* UAuraAbilitySystemFunctionLibrary::GetAttributeMenuWidgetController(
+	const UObject* WorldContextObject)
+{
+	if(APlayerController* PlayerController = WorldContextObject->GetWorld()->GetFirstPlayerController())
+	{
+		if (AAuraHUD* AuraHUD = Cast<AAuraHUD>(PlayerController->GetHUD()))
+		{
+			AAuraPlayerState* PS = PlayerController->GetPlayerState<AAuraPlayerState>();
+			UAbilitySystemComponent* ASC = PS->GetAbilitySystemComponent();
+			UAttributeSet* AS = PS->GetAttributeSet();
+
+			const FWidgetControllerParams WidgetControllerParams(PlayerController, PS, ASC, AS);
+
+			return AuraHUD->GetAttributeMenuWidgetController(WidgetControllerParams);
+		}
+	}
+
+	return nullptr;
+}
