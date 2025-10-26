6f562eef7db936c3be92f96e5a1a051042262f8e
UmiKami <ggoku02@gmail.com>
2025-10-26 11:14:28 -0400

    Created data asset that stores attribute tags and respective information to later display on widgets.

---
+++ Source/Aura/Private/AbilitySystem/Data/AttributeInfo.cpp
@@ -0,0 +1,81 @@
+﻿// Copyright By UmiKami
+
+
+#include "AbilitySystem/Data/AttributeInfo.h"
+
+#include "AuraGameplayTags.h"
+#include "UObject/ObjectSaveContext.h"
+
+FAuraAttributeInfo UAttributeInfo::FindAttributeInfoForTag(const FGameplayTag& AttributeTag, bool bLogNotFound) const
+{
+	if (const FAuraAttributeInfo* Info = AttributeInformation.Find(AttributeTag))
+	{
+		return *Info;
+	}
+
+	if (bLogNotFound)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Can't find Info for AttributeTag [%s] on AttributeInfo [%s]"),
+		       *AttributeTag.ToString(), *GetNameSafe(this));
+	}
+
+	return FAuraAttributeInfo();
+}
+
+
+// region Editor Utility to Autopopulate info on data asset
+
+void UAttributeInfo::SyncKeysToStructs()
+{
+	for (TPair<FGameplayTag, FAuraAttributeInfo>& Pair : AttributeInformation)
+	{
+		Pair.Value.AttributeTag = Pair.Key;
+
+		if (const TSharedPtr<FGameplayTagNode> GameplayTagNode = UGameplayTagsManager::Get().FindTagNode(
+			Pair.Value.AttributeTag))
+		{
+			Pair.Value.AttributeName = FText::FromName(GameplayTagNode->GetSimpleTagName());
+			Pair.Value.AttributeDescription = FText::FromString(GameplayTagNode->GetDevComment());
+		}
+	}
+}
+
+#if WITH_EDITOR
+void UAttributeInfo::PostEditChangeChainProperty(FPropertyChangedChainEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeChainProperty(PropertyChangedEvent);
+
+	if (PropertyChangedEvent.PropertyChain.GetActiveNode() &&
+		PropertyChangedEvent.PropertyChain.GetActiveNode()->GetValue() &&
+		PropertyChangedEvent.PropertyChain.GetActiveNode()->GetValue()->GetFName() ==
+		GET_MEMBER_NAME_CHECKED(UAttributeInfo, AttributeInformation))
+	{
+		SyncKeysToStructs();
+	}
+}
+
+void UAttributeInfo::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+
+	if (PropertyChangedEvent.MemberProperty && PropertyChangedEvent.MemberProperty->GetFName() ==
+		GET_MEMBER_NAME_CHECKED(UAttributeInfo, AttributeInformation))
+	{
+		SyncKeysToStructs();
+	}
+}
+#endif
+
+void UAttributeInfo::PostLoad()
+{
+	Super::PostLoad();
+	SyncKeysToStructs();
+}
+
+void UAttributeInfo::PreSave(FObjectPreSaveContext SaveContext)
+{
+	Super::PreSave(SaveContext);
+	SyncKeysToStructs();
+}
+
+// endregion
---
+++ Source/Aura/Public/AbilitySystem/Data/AttributeInfo.h
@@ -0,0 +1,54 @@
+﻿// Copyright By UmiKami
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Engine/DataAsset.h"
+#include "AttributeInfo.generated.h"
+
+USTRUCT(BlueprintType)
+struct FAuraAttributeInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FGameplayTag AttributeTag = FGameplayTag();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FText AttributeName = FText();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FText AttributeDescription = FText();
+
+	UPROPERTY(BlueprintReadOnly)
+	float AttributeValue = 0.f;
+};
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAttributeInfo : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	FAuraAttributeInfo FindAttributeInfoForTag(const FGameplayTag& AttributeTag, bool bLogNotFound = false) const;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TMap<FGameplayTag, FAuraAttributeInfo> AttributeInformation;
+
+private:
+	void SyncKeysToStructs();
+
+#if WITH_EDITOR
+
+public:
+	virtual void PostEditChangeChainProperty(FPropertyChangedChainEvent& PropertyChangedEvent) override;
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif
+
+	virtual void PostLoad() override;
+	virtual void PreSave(FObjectPreSaveContext SaveContext) override;
+};
