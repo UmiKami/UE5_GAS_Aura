07edd3241e6fc036a43cef0456815bd5309fe0a2
UmiKami <ggoku02@gmail.com>
2025-11-06 22:44:45 -0500

    Attribute Menu Controller now loops through all attributes on attribute info data asset and broadcast a AttributeInfo struct for every attribute.

--- Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -3,9 +3,23 @@
 
 #include "UI/WidgetController/AttributeMenuWidgetController.h"
 
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "AbilitySystem/Data/AttributeInfo.h"
+
 void UAttributeMenuWidgetController::BroadcastInitialValues()
 {
+	UAuraAttributeSet* AS = CastChecked<UAuraAttributeSet>(AttributeSet);
+
+	checkf(AttributeInfoDA, TEXT("Attribute info has not been set on Attribute Menu Widget Controller"));
+
+	for (auto& Pair : AttributeInfoDA->AttributeInformation)
+	{
+		FAuraAttributeInfo NewAttributeInfo = AttributeInfoDA->FindAttributeInfoForTag(Pair.Key);
+		
+		NewAttributeInfo.AttributeValue = NewAttributeInfo.GameplayAttribute.GetNumericValue(AS);
 	
+		AttributeInfoSignature.Broadcast(NewAttributeInfo);
+	}
 }
 
 void UAttributeMenuWidgetController::BindCallbacksToDependencies()
