7dc7ee747be55d70616b26d33dfb01df857185e6
UmiKami <ggoku02@gmail.com>
2025-11-09 15:34:41 -0500

    Created Data Asset to Relate Input Actions to Tags

---
+++ Source/Aura/Private/Input/AuraInputConfig.cpp
@@ -0,0 +1,17 @@
+﻿// Copyright By UmiKami
+
+
+#include "Input/AuraInputConfig.h"
+
+const UInputAction* UAuraInputConfig::FindAbilityInputActionForTag(
+	const FGameplayTag& InputTag, bool bLogNotFound) const
+{
+	const UInputAction* FoundInputAction = *AbilityInputActions.Find(InputTag);
+
+	if (!FoundInputAction && bLogNotFound)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Cannot find input action for given tag: %s"), *InputTag.GetTagName().ToString());
+	}
+
+	return FoundInputAction;
+}
