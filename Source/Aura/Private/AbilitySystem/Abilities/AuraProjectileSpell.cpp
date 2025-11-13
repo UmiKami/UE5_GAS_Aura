f78bcd49e01ea3c19bf7fb0a933e4d18502a4290
UmiKami <ggoku02@gmail.com>
2025-11-13 14:37:32 -0500

    Added gameplay ability for firebolt

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -0,0 +1,49 @@
+﻿// Copyright By UmiKami
+
+
+#include "AbilitySystem/Abilities/AuraProjectileSpell.h"
+
+#include "Actor/AuraProjectile.h"
+#include "Interaction/CombatInterface.h"
+
+
+void UAuraProjectileSpell::SpawnProjectile()
+{
+	FGameplayAbilityActivationInfo AInfo = GetCurrentActivationInfo();
+
+	if (!HasAuthority(&AInfo))
+	{
+		return;
+	}
+
+	ICombatInterface* CombatInterface = Cast<ICombatInterface>(GetAvatarActorFromActorInfo());
+
+	if (CombatInterface)
+	{
+		const FVector SocketLocation = CombatInterface->GetCombatSocketLocation();
+
+		FTransform SpawnTransform;
+		SpawnTransform.SetLocation(SocketLocation);
+		// TODO: Set projectile rotation
+
+		AAuraProjectile* Projectile = GetWorld()->SpawnActorDeferred<AAuraProjectile>(
+			ProjectileClass,
+			SpawnTransform,
+			GetOwningActorFromActorInfo(),
+			Cast<APawn>(GetAvatarActorFromActorInfo()),
+			ESpawnActorCollisionHandlingMethod::AlwaysSpawn
+		);
+
+		// TODO: Give the projectile a Gameplay Effect Spec for causing damage
+
+		Projectile->FinishSpawning(SpawnTransform);
+	}
+}
+
+void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+                                           const FGameplayAbilityActorInfo* ActorInfo,
+                                           const FGameplayAbilityActivationInfo ActivationInfo,
+                                           const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+}
---
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraProjectileSpell.h
@@ -0,0 +1,28 @@
+﻿// Copyright By UmiKami
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraGameplayAbility.h"
+#include "AuraProjectileSpell.generated.h"
+
+class AAuraProjectile;
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraProjectileSpell : public UAuraGameplayAbility
+{
+	GENERATED_BODY()
+
+protected:
+	UFUNCTION(BlueprintCallable, Category="Projectile")
+	void SpawnProjectile();
+
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+	                             const FGameplayAbilityActivationInfo ActivationInfo,
+	                             const FGameplayEventData* TriggerEventData) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TSubclassOf<AAuraProjectile> ProjectileClass;
+};
