a321cc4def61650ca6e8a83e8589da7a630758ca
UmiKami <ggoku02@gmail.com>
2025-11-13 14:35:43 -0500

    added projectile actor to be spawn for projectile type abilities

---
+++ Source/Aura/Private/Actor/AuraProjectile.cpp
@@ -0,0 +1,42 @@
+﻿// Copyright By UmiKami
+
+
+#include "Actor/AuraProjectile.h"
+
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+
+// Sets default values
+AAuraProjectile::AAuraProjectile()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	bReplicates = true;
+
+	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
+
+	SetRootComponent(Sphere);
+
+	Sphere->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	Sphere->SetCollisionResponseToAllChannels(ECR_Ignore);
+	Sphere->SetCollisionResponseToChannel(ECC_WorldDynamic, ECR_Overlap);
+	Sphere->SetCollisionResponseToChannel(ECC_WorldStatic, ECR_Overlap);
+	Sphere->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+
+	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>("ProjectileMovement");
+	ProjectileMovementComponent->InitialSpeed = 550;
+	ProjectileMovementComponent->MaxSpeed = 550;
+	ProjectileMovementComponent->ProjectileGravityScale = 0;
+}
+
+void AAuraProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+	Sphere->OnComponentBeginOverlap.AddDynamic(this, &AAuraProjectile::OnSphereOverlap);
+}
+
+void AAuraProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+                                      UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+                                      const FHitResult& SweepResult)
+{
+}
