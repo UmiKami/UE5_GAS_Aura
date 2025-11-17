9b43096a27713dbd8c7683fb2217b8e2a3b11159
UmiKami <ggoku02@gmail.com>
2025-11-17 09:07:33 -0500

    Turned off generation of events in Capsule Component + minor code refactor

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -6,6 +6,7 @@
 #include "AbilitySystemComponent.h"
 #include "AbilitySystem/AuraAbilitySystemComponent.h"
 #include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Player/AuraPlayerController.h"
@@ -22,10 +23,14 @@ AAuraCharacter::AAuraCharacter()
 
 	Camera->SetupAttachment(SpringArm);
 
-	GetCharacterMovement()->bOrientRotationToMovement = true;
-	GetCharacterMovement()->RotationRate = FRotator(0.f, 400.f, 0.f);
-	GetCharacterMovement()->bConstrainToPlane = true;
-	GetCharacterMovement()->bSnapToPlaneAtStart = true;
+	GetCapsuleComponent()->SetGenerateOverlapEvents(false);
+
+	UCharacterMovementComponent* MovementComponent = GetCharacterMovement();
+	MovementComponent->bOrientRotationToMovement = true;
+	MovementComponent->RotationRate = FRotator(0.f, 400.f, 0.f);
+	MovementComponent->bConstrainToPlane = true;
+	MovementComponent->bSnapToPlaneAtStart = true;
+
 
 	bUseControllerRotationPitch = false;
 	bUseControllerRotationYaw = false;
