00e7723632be0179c73cf2718df7e82c05efa436
UmiKami <ggoku02@gmail.com>
2025-11-18 13:07:30 -0500

    Added widget component health bar and binded to health and max health attribute changes.

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -7,6 +7,10 @@
 #include "Interaction/EnemyInterface.h"
 #include "AuraEnemy.generated.h"
 
+struct FAuraAttributeInfo;
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FHealthAttributeChangeSignature, float, NewAttributeValue);
+
+class UWidgetComponent;
 /**
  * 
  */
@@ -17,7 +21,7 @@ class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 
 public:
 	AAuraEnemy();
-	
+
 #pragma region Enemy Interfcace
 	virtual void HighlightActor() override;
 	virtual void UnHighlightActor() override;
@@ -25,8 +29,13 @@ public:
 
 #pragma region Combat Interface
 	virtual int32 GetPlayerLevel() override;
-#pragma endregion 
-	
+#pragma endregion
+	UPROPERTY(BlueprintAssignable, Category="GAS|Attributes")
+	FHealthAttributeChangeSignature HealthSignature;
+
+	UPROPERTY(BlueprintAssignable, Category="GAS|Attributes")
+	FHealthAttributeChangeSignature MaxHealthSignature;
+
 protected:
 	virtual void BeginPlay() override;
 
@@ -34,5 +43,11 @@ protected:
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Character Class Defaults")
 	int32 Level = 1;
-	
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UWidgetComponent* HealthBarComponent;
+
+private:
+	void BroadcastInitialValues() const;
+	void BindCallbacksToChangesOnHealth();
 };
