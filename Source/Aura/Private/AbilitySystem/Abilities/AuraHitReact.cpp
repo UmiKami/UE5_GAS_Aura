8ca932297b7b78719de5b7f794ac359edce0175d
UmiKami <ggoku02@gmail.com>
2025-11-25 12:48:59 -0500

    Created gameplay ability to apply hit react cues

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraHitReact.cpp
@@ -0,0 +1,31 @@
+﻿// Copyright By UmiKami
+
+
+#include "AbilitySystem/Abilities/AuraHitReact.h"
+
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+
+void UAuraHitReact::ApplyHitReactGameplayEffect() const
+{
+	checkf(HitReactEffectClass, TEXT("Please set the gameplay effect for HitReact on AuraHitReact Ability."))
+
+	if (UAuraAbilitySystemComponent* AuraASC = Cast<UAuraAbilitySystemComponent>(
+		GetAbilitySystemComponentFromActorInfo()))
+	{
+		FGameplayEffectContextHandle EffectContext = AuraASC->MakeEffectContext();
+		EffectContext.AddSourceObject(GetAvatarActorFromActorInfo());
+
+		const FGameplayEffectSpecHandle SpecHandle = AuraASC->MakeOutgoingSpec(HitReactEffectClass, 1.f, EffectContext);
+
+		AuraASC->ApplyGameplayEffectSpecToSelf(*SpecHandle.Data);
+	}
+}
+
+void UAuraHitReact::RemoveHitReactGameplayEffect() const
+{
+	if (UAuraAbilitySystemComponent* AuraASC = Cast<UAuraAbilitySystemComponent>(
+	GetAbilitySystemComponentFromActorInfo()))
+	{
+		AuraASC->RemoveActiveGameplayEffectBySourceEffect(HitReactEffectClass, AuraASC);
+	}
+}
---
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraHitReact.h
@@ -0,0 +1,26 @@
+﻿// Copyright By UmiKami
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraGameplayAbility.h"
+#include "AuraHitReact.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraHitReact : public UAuraGameplayAbility
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TSubclassOf<UGameplayEffect> HitReactEffectClass;
+
+	UFUNCTION(BlueprintCallable, Category="Combat")
+	void ApplyHitReactGameplayEffect() const;
+	
+	UFUNCTION(BlueprintCallable, Category="Combat")
+	void RemoveHitReactGameplayEffect() const;
+};
