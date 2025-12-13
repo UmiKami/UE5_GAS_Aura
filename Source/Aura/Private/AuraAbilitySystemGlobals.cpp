4e6f5c3244f766eab14733a77f60a7e8a0377cb6
UmiKami <ggoku02@gmail.com>
2025-12-13 11:25:50 -0500

    Created cystin gameplay effect and added new members to store info

---
+++ Source/Aura/Private/AuraAbilitySystemGlobals.cpp
@@ -0,0 +1,11 @@
+﻿// Copyright By UmiKami
+
+
+#include "AuraAbilitySystemGlobals.h"
+
+#include "AuraAbilityTypes.h"
+
+FGameplayEffectContext* UAuraAbilitySystemGlobals::AllocGameplayEffectContext() const
+{
+	return new FAuraGameplayEffectContext();
+}
---
+++ Source/Aura/Public/AuraAbilityTypes.h
@@ -0,0 +1,54 @@
+﻿#pragma once
+#include "GameplayEffectTypes.h"
+#include "AuraAbilityTypes.generated.h"
+
+USTRUCT(BlueprintType)
+struct FAuraGameplayEffectContext : public FGameplayEffectContext
+{
+	GENERATED_BODY()
+
+	bool IsCriticalHit() const { return bCriticalHit; }
+	bool IsBlockedHit() const { return bBlockedHit; }
+
+	void SetIsCriticalHit(const bool bInIsCriticalHit) { bCriticalHit = bInIsCriticalHit; }
+	void SetIsBlockedHit(const bool bInIsBlockedHit) { bBlockedHit = bInIsBlockedHit; }
+
+	/** Returns the actual struct used for serialization, subclasses must override this! */
+	virtual UScriptStruct* GetScriptStruct() const override
+	{
+		return StaticStruct();
+	};
+
+	virtual FAuraGameplayEffectContext* Duplicate() const override
+	{
+		FAuraGameplayEffectContext* NewContext = new FAuraGameplayEffectContext();
+		*NewContext = *this;
+		if (GetHitResult())
+		{
+			// Does a deep copy of the hit result
+			NewContext->AddHitResult(*GetHitResult(), true);
+		}
+		return NewContext;
+	};
+
+	/** Custom serialization, subclasses must override this */
+	virtual bool NetSerialize(FArchive& Ar, UPackageMap* Map, bool& bOutSuccess) override;
+
+protected:
+	UPROPERTY()
+	bool bBlockedHit = false;
+
+	UPROPERTY()
+	bool bCriticalHit = false;
+};
+
+
+template <>
+struct TStructOpsTypeTraits<FAuraGameplayEffectContext> : TStructOpsTypeTraitsBase2<FAuraGameplayEffectContext>
+{
+	enum
+	{
+		WithNetSerializer = true,
+		WithCopy = true
+	};
+};
